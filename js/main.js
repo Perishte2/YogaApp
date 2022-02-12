ba63dcac9fb466303373b404aee461696e303a7f
Perishte <perishte010203@gmail.com>
2022-02-12 15:34:41 +0600

    Added classOptions and created new DIv through DOM

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 let age = document.getElementById('age');
 
 function showUser(surname, name) {
@@ -5,3 +6,29 @@ function showUser(surname, name) {
 alert("Пользователь " + surname + " " + name + ", его возраст " + this.value );
 }
 showUser.apply(age, ['Горький', ' Максим']);
+
+*/
+
+
+
+//2 task
+class Options {
+	constructor(height, width, bg, fontSize, textAlign) {
+		this.height = height;
+		this.width = width;
+		this.bg = bg;
+		this.fontSize = fontSize;
+		this.textAlign = textAlign;
+	}
+
+	newDiv() {
+		let elem = document.createElement('div');
+		document.body.appendChild(elem);
+		let params = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+		elem.style.cssText = params;
+	}
+}
+
+const item = new Options(300, 350, "blue", 14, "center");
+
+item.newDiv();
